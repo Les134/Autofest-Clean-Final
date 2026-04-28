9dfe1d2f9026599b4e7e0f084f636fd7e5157915
Les134 <admin@autofest.com.au>
2026-04-28 13:44:21 +1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import ScoreSheet from "./ScoreSheet";
 import Leaderboard from "./Leaderboard";
 
-export default function ScoreSheet App() {
+export default function App() {
   const [eventName, setEventName] = useState("");
   const [judgeName, setJudgeName] = useState("");
   const [started, setStarted] = useState(false);
