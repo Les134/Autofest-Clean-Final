34e861b0babfe1e4cdc97dbba2225b3ad830fc3c
Les134 <admin@autofest.com.au>
2026-04-29 10:30:30 +1000

    Update ScoreSheet.js

--- src/ScoreSheet.js
+++ src/ScoreSheet.js
@@ -1,4 +1,5 @@
-  import React, { useState, useEffect } from "react";
+import { db } from "./firebase";
+import React, { useState, useEffect } from "react";
 
 export default function ScoreSheet({ eventName, judgeName }) {
   const [carName, setCarName] = useState("");
